a43ac475173a2569916fb26a2768a81e4a7fdecd
Niraj Patil <nirajpatil@gmail.com>
2013-03-03 14:53:42 -0800

    Added hidden intro and anotehr set of starts for CSS3 animation.

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -15,8 +15,74 @@ $(window).load(function() {
 	});
 
 
+/* Home Page Animation Sequence
+------------------------------------------------*/
+
+//1. Logo animates on hover (whoa awesome!)
+$('.logo').hover(function(){
+
+	$(this).addClass('animated dip');
+
+}, function(){
+
+	$(this).removeClass('animated dip');
+
+});
+
+//2. Stars fade in.
+$('#page-home').waypoint(function() {
+	
+	$('.stars.front').addClass('animated fall');
+
+});
+
+//3. More stars fade in.
+$('#page-home .header p').waypoint(function() {
+
+	$('.stars.back').addClass('animated fall');
+
+});
+
+//4. First row of tickets fade in
+$('#page-home .manmadeit small').waypoint(function(down) {
+
+	$('.ticket.top').addClass('animated fall');
+
+});
+
+//4. Second row of tickets fade in
+$('#page-home .manmadeit .water').waypoint(function(down) {
+
+	$('.ticket.middle').addClass('animated fall');
+
+});
+
+//4. Third row of tickets fade in
+$('#page-home .redbox').waypoint(function(down) {
+
+	$('.ticket.bottom').addClass('animated fall');
+
+});
+
+//5. Third row of tickets fade in
+$('#page-home .redbox').waypoint(function(down) {
+
+	$('.ticket.bottom').addClass('animated fall');
+
+});
+
+//5. Third row of tickets fade in
+$('#page-home .redbox').waypoint(function(down) {
+
+	$('.ticket.bottom').addClass('animated fall');
+
+});
+
+
+
 /* Fading out galaxy on scroll
 ------------------------------------------------*/
+
 
 	$('#page-sparkart .group').waypoint(function(down) {
 
@@ -31,6 +97,8 @@ $(window).load(function() {
 		$('.galaxy .ring').addClass('fade-to-white');
 
 	});
+
+
 
 
 /* End Load
